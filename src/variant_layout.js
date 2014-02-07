df04c7edaf88051f0f3f76a75555143b277b0a69
kleinone <kleinone@systemsbiology.org>
2014-02-06 20:09:44 -0800

    Added stub for layout support object

---
+++ src/variant_layout.js
@@ -0,0 +1,17 @@
+define (
+[
+
+],
+function (
+
+) {
+    var VariantLayoutPrototype = {
+        add_track_data: function(data) {
+
+        },
+
+        regions: function(region_data) {
+
+        }
+    };
+});
