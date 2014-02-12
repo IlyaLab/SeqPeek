f1e03b3c2f1254b861e5880bc4ae523b40d021aa
kleinone <kleinone@systemsbiology.org>
2014-02-11 18:19:25 -0800

    Scaling functions in separate file

--- src/seqpeek_scaling.js
+++ src/seqpeek_scaling.js
@@ -7,20 +7,77 @@ function(
     _
 ) {
     return {
-        createFractionalBars: function(track, color_by, samples_by_categories, param_statistics) {
+        createFractionalBars: function(samples_by_categories, render_info) {
             var total_samples = _.reduce(samples_by_categories, function(memo, value, key) {
                 return memo + value;
             }, 0);
 
-            return _.reduce(color_by.group_names, function(memo, group_name) {
-                if (_.has(samples_by_categories, group_name)) {
-                    var number = samples_by_categories[group_name],
-                        fract = number / total_samples,
-                        height = fract * color_by.max_height;
+            return _.reduce(_.keys(samples_by_categories, function(memo, group_name) {
+                var number = samples_by_categories[group_name],
+                    fract = number / total_samples,
+                    height = fract * render_info.max_height;
 
+                memo.array.push({
+                    height: height,
+                    color: color_scale(group_name),
+                    y: memo.current_y
+                });
+
+                memo.current_y += height;
+
+                return memo;
+            }, {
+                array: [],
+                current_y: 0
+            }));
+        },
+
+        createLinearBars: function(samples_by_categories, param_statistics, render_info) {
+            var pixels_per_sample = 1,
+                category_max_height = render_info.max_height / _.keys(samples_by_categories).length;
+
+            var log_scale = d3.scale
+                .log().base([2])
+                .domain([1.0, param_statistics.max_samples_in_location])
+                .range([5, category_max_height]);
+
+            return _.reduce(_.keys(samples_by_categories, function(memo, group_name) {
+                var number = samples_by_categories[group_name],
+                    height = log_scale(d3.max([1.0, number]));
+
+                if (number > 0) {
                     memo.array.push({
                         height: height,
-                        color: color_scale(group_name),
+                        color: render_info.category_colors(group_name),
+                        y: memo.current_y
+                    });
+
+                    memo.current_y += height;
+                }
+
+                return memo;
+            }, {
+                array: [],
+                current_y: 0
+            }));
+        },
+
+        createLog2Bars: function(samples_by_categories, param_statistics, render_info) {
+            var category_max_height = render_info.max_height / _.keys(samples_by_categories).length;
+
+            var log_scale = d3.scale
+                .log().base([2])
+                .domain([1.0, param_statistics.max_samples_in_location])
+                .range([5, category_max_height]);
+
+            var bars = _.reduce(_.keys(samples_by_categories), function(memo, group_name) {
+                var number = samples_by_categories[group_name],
+                    height = log_scale(d3.max([1.0, number]));
+
+                if (number > 0) {
+                    memo.array.push({
+                        height: height,
+                        color: render_info.category_colors(group_name),
                         y: memo.current_y
                     });
 
@@ -32,82 +89,17 @@ function(
                 array: [],
                 current_y: 0
             });
-        },
-
-        createLinearBars: function(track, color_by, samples_by_categories, param_statistics) {
-            var pixels_per_sample = 1,
-                category_max_height = color_by.max_height / _.keys(samples_by_categories).length;
-
-            var log_scale = d3.scale
-                .log().base([2])
-                .domain([1.0, param_statistics.max_samples_in_location])
-                .range([5, category_max_height]);
-
-            var bars = _.reduce(color_by.group_names, function(memo, group_name) {
-                if (_.has(samples_by_categories, group_name)) {
-                    var number = samples_by_categories[group_name],
-                        height = log_scale(d3.max([1.0, number]));
-
-                    if (number > 0) {
-                        memo.array.push({
-                            height: height,
-                            color: color_by.color_scale(group_name),
-                            y: memo.current_y
-                        });
-
-                        memo.current_y += height;
-                    }
-                }
-
-                return memo;
-            }, {
-                array: [],
-                current_y: 0
-            });
 
             return bars;
         },
 
-        createLog2Bars: function(track, color_by, samples_by_categories, param_statistics) {
-            var category_max_height = color_by.max_height / _.keys(samples_by_categories).length;
+        createNormalizedLinearBars: function(param_data, samples_by_categories, render_info) {
+            var category_sizes = param_data.statistics.by_category,
+                min_height = render_info.min_height,
+                max_height = render_info.max_height,
+                pixels_per_sample = render_info.pixels_per_sample;
 
-            var log_scale = d3.scale
-                .log().base([2])
-                .domain([1.0, param_statistics.max_samples_in_location])
-                .range([5, category_max_height]);
-
-            var bars = _.reduce(color_by.group_names, function(memo, group_name) {
-                if (_.has(samples_by_categories, group_name)) {
-                    var number = samples_by_categories[group_name],
-                        height = log_scale(d3.max([1.0, number]));
-
-                    if (number > 0) {
-                        memo.array.push({
-                            height: height,
-                            color: color_by.color_scale(group_name),
-                            y: memo.current_y
-                        });
-
-                        memo.current_y += height;
-                    }
-                }
-
-                return memo;
-            }, {
-                array: [],
-                current_y: 0
-            });
-
-            return bars;
-        },
-
-        createNormalizedLinearBars: function(track, color_by, samples_by_categories, param_statistics) {
-            var category_sizes = color_by.category_sizes,
-                min_height = color_by.min_height,
-                max_height = color_by.max_height,
-                pixels_per_sample = color_by.pixels_per_sample;
-
-            var bars = _.reduce(color_by.group_names, function(memo, group_name) {
+            var bars = _.reduce(_.keys(samples_by_categories), function(memo, group_name) {
                 if (_.has(samples_by_categories, group_name)) {
                     var number = samples_by_categories[group_name],
                         cat_size = category_sizes[group_name],
@@ -123,8 +115,8 @@ function(
                     if (number > 0) {
                         memo.array.push({
                             height: height,
-                            color: color_by.color_scale(group_name),
-                            y: memo.current_y
+                            y: memo.current_y,
+                            color: render_info.category_colors[group_name]
                         });
 
                         memo.current_y += height;
@@ -147,8 +139,8 @@ function(
 
                     memo.array.push({
                         height: height,
-                        color: bar.color,
-                        y: memo.current_y
+                        y: memo.current_y,
+                        color: bar.color
                     });
 
                     memo.current_y += height;
@@ -162,59 +154,7 @@ function(
             else {
                 return bars;
             }
-        },
-
-        createNormalizedLogBars: function(track, color_by, samples_by_categories, param_statistics) {
-            var category_sizes = color_by.category_sizes,
-                category_max_height = color_by.max_height / _.keys(category_sizes).length;
-
-            var log_scale = d3.scale
-                .log().base([2])
-                .domain([1.0, color_by.category_sum])
-                .range([5, color_by.max_height]);
-
-            var bars = _.reduce(color_by.group_names, function(memo, group_name) {
-                if (_.has(samples_by_categories, group_name)) {
-                    var number = samples_by_categories[group_name],
-                        cat_size = category_sizes[group_name],
-                        height = d3.max([1.0, category_max_height * (number / cat_size)]);
-
-                    if (number > 0) {
-                        memo.array.push({
-                            height: height,
-                            color: color_by.color_scale(group_name),
-                            y: memo.current_y
-                        });
-
-                        memo.current_y += height;
-                    }
-                }
-
-                return memo;
-            }, {
-                array: [],
-                current_y: 0
-            });
-
-            var total_height = log_scale(bars.current_y),
-                k = total_height / bars.current_y;
-
-            return _.reduce(bars.array, function(memo, bar) {
-                var height = k * bar.height;
-
-                memo.array.push({
-                    height: height,
-                    color: bar.color,
-                    y: memo.current_y
-                });
-
-                memo.current_y += height;
-
-                return memo;
-            },{
-                array: [],
-                current_y: 0
-            });
         }
+
     }
 });
--- src/tracks/bar_plot_track.js
+++ src/tracks/bar_plot_track.js
@@ -5,88 +5,33 @@ define   (
     'underscore',
 
     '../util/data_adapters',
-    '../util/gene_region_utils'
+    '../util/gene_region_utils',
+    '../seqpeek_scaling'
 ],
 function(
     d3,
     vq,
     _,
     DataAdapters,
-    GeneRegionUtils
+    GeneRegionUtils,
+    ScalingFunctions
 ) {
-    _createNormalizedLinearBars = function(param_data, samples_by_categories, category_colors) {
-        var category_sizes = param_data.statistics.by_category,
-            min_height = 10.0,
-            max_height = 120.0,
-            pixels_per_sample = 200.0;
-
-        var bars = _.reduce(_.keys(samples_by_categories), function(memo, group_name) {
-            if (_.has(samples_by_categories, group_name)) {
-                var number = samples_by_categories[group_name],
-                    cat_size = category_sizes[group_name],
-                    height;
-
-                if (cat_size !== undefined && cat_size > 0) {
-                    height = pixels_per_sample * (number / cat_size)
-                }
-                else {
-                    height = 0;
-                }
-
-                if (number > 0) {
-                    memo.array.push({
-                        height: height,
-                        y: memo.current_y,
-                        color: category_colors[group_name]
-                    });
-
-                    memo.current_y += height;
-                }
-            }
-
-            return memo;
-        }, {
-            array: [],
-            current_y: 0
-        });
-
-        // Scale if needed
-        if (bars.current_y <= min_height || max_height <= bars.current_y) {
-            var total_height = d3.min([d3.max([bars.current_y, min_height]), max_height]),
-                k = total_height / bars.current_y;
-
-            return _.reduce(bars.array, function(memo, bar) {
-                var height = k * bar.height;
-
-                memo.array.push({
-                    height: height,
-                    y: memo.current_y,
-                    color: bar.color
-                });
-
-                memo.current_y += height;
-
-                return memo;
-            },{
-                array: [],
-                current_y: 0
-            });
-        }
-        else {
-            return bars;
-        }
-    };
-
     var BarPlotTrackPrototype = {
         getHeight: function() {
             return this.dimensions.height;
         },
 
         _applyStackedBarRenderData: function(data, data_info) {
-            var self = this;
+            var self = this,
+                render_info = {
+                    min_height: 10.0,
+                    max_height: 120.0,
+                    pixels_per_sample: 200,
+                    category_colors: self.config.color_scheme
+                };
 
             DataAdapters.apply_to_variant_types(data, function(type_data, memo) {
-                type_data.render_data = _createNormalizedLinearBars(type_data, type_data.statistics.by_category, self.config.color_scheme);
+                type_data.render_data = ScalingFunctions.createNormalizedLinearBars(type_data, type_data.statistics.by_category, render_info);
             });
         },
 
