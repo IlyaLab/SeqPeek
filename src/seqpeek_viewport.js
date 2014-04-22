f62a9c10806ab342f1c25ee6c9e4147747e4fcc1
kleinone <kleinone@systemsbiology.org>
2014-04-22 16:55:54 -0700

    Added scale variable to viewport

--- src/seqpeek_viewport.js
+++ src/seqpeek_viewport.js
@@ -7,13 +7,19 @@ function (
 ) {
     var prototype = {
         _getCoordinateFromScaleLocation: function(x) {
-            return this.region_layout.getCoordinateFromScreenLocation(x);
+            return this.region_layout.getCoordinateFromScreenLocation(x) / this.viewport_scale;
+        },
+
+        _getScaleLocationFromCoordinate: function(coordinate) {
+            return this.region_layout.getScreenLocationFromCoordinate(coordinate) * this.viewport_scale;
         },
 
         _getVisibleCoordinates: function() {
+            // Calculate the coordinate range that is visible on the screen
             var start_x = -this.viewport_pos.x,
+                scaled_width = this.width * this.viewport_scale,
                 min_x = d3.max([start_x, 0]),
-                max_x = d3.min([start_x + this.width, this.width]);
+                max_x = d3.min([start_x + scaled_width, scaled_width]);
 
             var start = this._getCoordinateFromScaleLocation(min_x);
             var end = this._getCoordinateFromScaleLocation(max_x);
@@ -37,6 +43,14 @@ function (
             this.viewport_pos = param;
         },
 
+        getViewportScale: function() {
+            return this.viewport_scale;
+        },
+
+        setViewportScale: function(param) {
+            this.viewport_scale = param;
+        },
+
         region_layout: function(value) {
             this.region_layout = value;
 
@@ -53,6 +67,7 @@ function (
                 x: 0,
                 y: 0
             };
+            obj.viewport_scale = 1.0;
             obj.width = width;
 
             return obj;
