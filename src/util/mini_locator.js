2e82f276f03d9e37dcafcefe32ef7c78ed724294
kleinone <kleinone@systemsbiology.org>
2014-07-01 13:28:15 -0700

    New layout for mini locator rendering

--- src/util/mini_locator.js
+++ src/util/mini_locator.js
@@ -1,14 +1,22 @@
 define   (
 [
     'd3',
-    'underscore',
-    'vq'
+    'underscore'
 ],
 function(
     d3,
-    _,
-    vq
+    _
 ) {
+    // Default colors
+    var DEFAULT_EXON_REGION_FILL = "rgba(170,170,170,1)";
+    var DEFAULT_NONCODING_REGION_FILL = "rgba(170,170,170,1)";
+    var DEFAULT_VIEWPORT_STROKE = "rgba(255,0,0,0.7)";
+
+    var DEFAULT_VIEWPORT_STROKE_WIDTH = 2.0;
+
+    var DEFAULT_REGION_HEIGHT = 10;
+    var DEFAULT_VIEWPORT_HEIGHT = 20;
+
     var MiniLocatorPrototype = {
         init: function() {
             this.config.width = this.config.canvas_el.width;
@@ -63,8 +71,8 @@ function(
             var ctx = this.ctx;
 
             var x = region.layout.screen_x;
-            var y = 0;
-            var h = this.config.height / 2.0;
+            var y = (this.config.height / 2.0) - (DEFAULT_REGION_HEIGHT / 2.0);
+            var h = DEFAULT_REGION_HEIGHT;
             var w = region.layout.screen_width;
 
             ctx.beginPath();
@@ -72,7 +80,7 @@ function(
             ctx.closePath();
 
             ctx.lineWidth = 0;
-            ctx.fillStyle = "gray";
+            ctx.fillStyle = DEFAULT_EXON_REGION_FILL;
             ctx.fill();
         },
 
@@ -80,8 +88,8 @@ function(
             var ctx = this.ctx;
 
             var x = region.layout.screen_x;
-            var y = 0;
-            var h = this.config.height / 2.0;
+            var y = (this.config.height / 2.0) - (DEFAULT_REGION_HEIGHT / 2.0);
+            var h = DEFAULT_REGION_HEIGHT;
             var w = region.layout.screen_width;
 
             ctx.beginPath();
@@ -89,25 +97,26 @@ function(
             ctx.closePath();
 
             ctx.lineWidth = 0;
-            ctx.fillStyle = "red";
+            ctx.fillStyle = DEFAULT_NONCODING_REGION_FILL;
             ctx.fill();
         },
 
         _renderViewportExtent: function(min_x, max_x) {
             var ctx = this.ctx;
             var scale = this.config.scale;
+            var adjust = (DEFAULT_VIEWPORT_STROKE_WIDTH / 2.0);
 
-            var x = scale * min_x;
-            var y = this.config.height / 2.0;
-            var h = this.config.height / 2.0;
-            var w = scale * (max_x - min_x);
+            var x = scale * min_x + adjust;
+            var y = (this.config.height / 2.0) - (DEFAULT_VIEWPORT_HEIGHT / 2.0) - (0.5 * adjust);
+            var h = DEFAULT_VIEWPORT_HEIGHT;
+            var w = scale * (max_x - min_x) - 2 * adjust;
 
             ctx.beginPath();
             ctx.rect(x, y, w, h);
             ctx.closePath();
 
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = "black";
+            ctx.lineWidth = DEFAULT_VIEWPORT_STROKE_WIDTH;
+            ctx.strokeStyle = DEFAULT_VIEWPORT_STROKE;
             ctx.stroke();
         }
     };
